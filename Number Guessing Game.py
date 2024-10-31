86567df98bcb0aab23f39f92bb67cc562e685bee
Asdmir786 <asmir.alams.com@gmail.com>
2024-10-31 19:53:05 +0500

    Confirm Done

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -7,18 +7,30 @@ lives = 3
 attempts = 0
     # Main File
 while True:
-    lives = int(input("How many Lives Do you want. "))
-    if lives < 50 and lives > 0:
-        break
-    else:
-        print("Enter a Number Which is lower than 50. ")
+    try:
+        lives = int(input("How many lives do you want (between 1 and 50): "))
+        if 1 <= lives <= 50: # same as lives >= 1 and lives <= 50
+            break
+        else:
+            print("Try again, Enter a number between 1 and 50.")
+    except ValueError:
+        print("Try again, Enter a valid number.")
+
 
 
 print("Generating A Number.... ")
 time.sleep(3)
 print("Number Generated ....")
 while lives!=0:
-    UserInput = int(input("Enter your Number to Guess.\n: "))
+    while True:
+        try:
+            UserInput = int(input("Enter your Number to Guess.\n: "))
+            if UserInput >=1 and UserInput <= 50:
+                break
+            else:
+                print("Babeo Number Deynai 50 or 1 ke darmiyaan. Chawlian marne nu nhi kaha.")
+        except ValueError:
+            print("Number not Bs. ")
     attempts += 1
     if UserInput == TheNumber:
         print(f"You Guessed the Number {TheNumber} in {attempts} attempts and {lives} left.")
